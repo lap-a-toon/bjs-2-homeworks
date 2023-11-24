802c1ceaaa770b47b4cbeb3bbea9fb425b45e6d7
lap-a-toon <lap-a-toon@yandex.ru>
2023-11-24 13:15:47 +0500

    не очень значительные правки к ДЗ №1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = Math.pow(b,2) - 4*a*c;
-  if(d<0){
-    // дискриминант < 0, корней нет
-  }else if(d===0){
-    // дискриминант = 0, корень один
-    arr.push(-b/(2*a));
-  }else{
+  const arr = [];
+  const d = Math.pow(b,2) - 4*a*c;
+  if(d>0){
     // дискриминант > 0, два корня
     arr.push((-b + Math.sqrt(d) )/(2*a));
     arr.push((-b - Math.sqrt(d) )/(2*a));
-  }
+  }else if(d===0){
+    // дискриминант = 0, корень один
+    arr.push(-b/(2*a));
+  } // дискриминант < 0, корней нет
+  
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let creditBody = amount - contribution;
-  let percentKoefficient = percent/(100*12);
-  let payment = creditBody * (percentKoefficient + (percentKoefficient / (Math.pow(1 + percentKoefficient, countMonths) - 1)))
-  let paymentToShow = Number((payment*countMonths).toFixed(2));
+  const creditBody = amount - contribution,
+        percentKoefficient = percent/(100*12),
+        payment = creditBody * (percentKoefficient + (percentKoefficient / (Math.pow(1 + percentKoefficient, countMonths) - 1))),
+        paymentToShow = Number((payment*countMonths).toFixed(2));
   return paymentToShow;
 }
