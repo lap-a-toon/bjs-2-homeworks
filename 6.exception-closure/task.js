6efd28347ad15cb78525c6eb983dc723be4052de
lap-a-toon <lap-a-toon@yandex.ru>
2023-12-16 23:36:43 +0500

    Задание 6. [Обработка исключений и замыкания]

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(numStr){
+    if(isNaN(numStr)){
+        throw new Error("Невалидное значение");
+    }else{
+        return Number.parseFloat(numStr);
+    }
+}
+function validateCount(toParse){
+    try{
+        return parseCount(toParse)
+    }catch(e){
+        return e;
+    }
+}
+
+
+class Triangle{
+    constructor(a,b,c){
+        if(a+b<c || a+c<b || b+c<a){
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+    get area(){
+        let p = .5*(this.perimeter); // полупериметр
+        return Number(Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c)).toFixed(3));
+    }
+}
+
+class TriangleWrong{
+    get perimeter(){
+        return "Ошибка! Треугольник не существует";
+    }
+    get area(){
+        return "Ошибка! Треугольник не существует";
+    }
+}
+
+function getTriangle(a,b,c){
+    try{
+        return new Triangle(a,b,c)
+    }catch{
+        return new TriangleWrong(a,b,c)
+    }
+}
